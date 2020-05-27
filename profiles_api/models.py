83e0c1b949373266efa565481684af6830f7da04
Zaid <zamurad97@yahoo.com>
2020-05-27 22:35:19 +0300

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+#These are the standard base classes you need when overriding the default django user models
+
+#This code is written based on the documentation through: https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#writing-a-manager-for-a-custom-user-model
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system""" #This is python standard for writing doc strings to describe classes and functions to describe them
+    email = models.EmailField(max_length=255, unique=True) #create an email column that is unique for each user
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True) #a field for permission system to determine is a user is active or not which allows us to deactivate users if we want in the future
+    is_staff = models.BooleanField(default=False) #allows us to change a user to be a staff or not to login to admin page
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
